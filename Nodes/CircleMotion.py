d6d181a5460099921ed1286c7bd38c5ec07c9414
RobWks <robertweeks94@gmail.com>
2016-07-20 14:29:28 -0400

    Updates

--- Nodes/CircleMotion.py
+++ Nodes/CircleMotion.py
@@ -11,16 +11,27 @@ __email__ = "rweeks@uwaterloo.ca"
 
 import rospy
 from geometry_msgs.msg import Twist
-#from geometry_msgs.msg import PoseWithCovarianceStamped
-#from tf.transformations import euler_from_quaternion
+from geometry_msgs.msg import PoseWithCovarianceStamped
+from tf.transformations import euler_from_quaternion
 
-def talker():
+def callback(data):
+	X = msg.pose.pose.position.x #Robot X psotition
+	Y = msg.pose.pose.position.y #Robot Y psotition
+	(roll,pitch,yaw) = euler_from_quaternion(msg.pose.pose.orientation)
+	rospy.loginfo("pose_callback X: {}} Y: {} Yaw: {}".format(X, Y, yaw))
+
+#def listener():
+
+
+def main():
 	# (roll,pitch,yaw) = euler_from_quaternion(quaternion)
 	#Initialize
 	rospy.init_node('CircleMotion', anonymous=True)
 
 	#Create publisher used to send out twist
 	pub = rospy.Publisher('/cmd_vel_mux/input/navi', Twist, queue_size=10)
+	rospy.Subscriber("/amcl_pose", Twist, callback)
+
 
 	#Set update rate to 20 Hz
 	rate = rospy.Rate(20) 
@@ -30,7 +41,7 @@ def talker():
 		# Twist is a datatype for velocity
 		move_cmd = Twist()
 		# let's go forward at 0.2 m/s
-		move_cmd.linear.x = 0.2
+		move_cmd.linear.x = 0.1
 		# let's turn at 0.2 radians/s
 		move_cmd.angular.z = 0.2
 		#publish twist
@@ -40,7 +51,7 @@ def talker():
 
 if __name__ == '__main__':
     try:
-        talker()
+        main()
     except rospy.ROSInterruptException:
         pass
 	
