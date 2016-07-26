4b8d7b35fcab57b8f784bea4444ed98a7147c124
RobWks <robertweeks94@gmail.com>
2016-07-25 22:07:32 -0400

    Updated Matlab Script

--- Nodes/MapReader.m
+++ Nodes/MapReader.m
@@ -1,5 +1,22 @@
-M = csvread('test.csv');
-map = reshape(M/100,[2250,768]);
-I = mat2gray(map,[0,1]);
-imwrite(I,'my_graphics_file.png','png')
+file = csvread('e5_smaller.csv');
+input = reshape(file/100,[2250/2,768/2]);
+map = im2bw(input,0.7);
+map = flipud(map);
+[M,N]= size(map);
+imagesc(map);
+hold on
 
+num_obstacles = 5000;
+x_o = round(rand(1,num_obstacles)*(N-1))+1;
+y_o = round(rand(1,num_obstacles)*(M-1))+1;
+
+
+for i=fliplr(1:length(x_o))
+    if ~map(y_o(i),x_o(i))
+        x_o(i) = [];
+        y_o(i) = [];
+    end
+end
+
+plot(x_o,y_o,'*')
+axis equal
